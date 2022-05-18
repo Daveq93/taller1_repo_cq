d40008d69952e88df2bbb7ef690a3dd255c9a8fd
Cristian <cdavidq.22@gmail.com>
2022-05-17 19:57:39 -0500

    Archivos necesarios para el proyecto

---
+++ src/main/java/ec/edu/uce/Taller1RepoCq1Application.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1RepoCq1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1RepoCq1Application.class, args);
+	}
+
+}
